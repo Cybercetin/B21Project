d824879c4c386335f4daa4a08d6169383954a586
Murat Cetin <mcetin@gmail.com>
2020-11-10 11:40:46 -0800

    Code updated

--- src/Day02Repo/Main.java
+++ src/Day02Repo/Main.java
@@ -2,8 +2,8 @@ package Day02Repo;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <=10; i++){
-            System.out.println(i);
+        for (int i = 0; i <=20; i++){
+            System.out.print(i + " ");
         }
 
     }
