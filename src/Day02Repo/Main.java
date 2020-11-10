4fd3256e6e991b31c3cba7e824a246db02918932
Murat Cetin <mcetin@gmail.com>
2020-11-10 12:26:25 -0800

    Code updated

--- src/Day02Repo/Main.java
+++ src/Day02Repo/Main.java
@@ -2,7 +2,7 @@ package Day02Repo;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <=20; i= i+2){
+        for (int i = 0; i <=20; i= i+5){
             System.out.print(i + " ");
             System.out.println("We are learning github and version control");
         }
